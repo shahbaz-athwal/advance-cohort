b94e2c620e17520a1b7ab98a1387ac5794fee8af
Shahbaz <shahbazathwal2107@gmail.com>
2024-06-21 21:09:08 +0530

    added react client

--- websockets/src/index.ts
+++ websockets/src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-import { WebSocketServer } from 'ws'
+import WebSocket ,{ WebSocketServer } from 'ws'
 
 const app = express()
 const httpServer = app.listen(8080)
+
+let userCount = 0
 
 const wss = new WebSocketServer({ server: httpServer });
 
@@ -10,12 +12,15 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
+    console.log('received: %s', data);
+    // wss.clients.forEach(function each(client) {
+    //   if (client.readyState === WebSocket.OPEN) {
+    //     client.send(data, { binary: isBinary });
+    //   }
+    // });
   });
-
+  console.log('Client connected',++userCount);
   ws.send('Hello! Message From Server!!');
+
+  
 });
